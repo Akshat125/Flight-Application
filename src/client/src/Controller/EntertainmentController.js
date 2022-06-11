fd6efb32f6e72d275f20debbac5ef84039252780
ge47raj <akshat.tandon@tum.de>
2022-06-11 18:34:40 +0200

    All the controllers are functional now

---
+++ src/client/src/Controller/EntertainmentController.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const ENTERTAINMENT_API_BASE_URL = "";
+
+class EntertainmentController {
+
+    // @Param:
+    /* @ret:
+     */
+    getMovie() {
+    }
+
+    // @Param:
+    /* @ret:
+     */
+    getMusic() {
+    }
+
+}
+
+export default new EntertainmentController()
