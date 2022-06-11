b92e4d2d2a44487f5eb7dadc24d868fa1e51e6de
ge42faz <ge42faz@mytum.de>
2022-06-12 01:36:53 +0200

    added clean basic homepage + navbar template with colour scheme; fonts, images, functionality pending

--- src/client/src/Components/Header/Subcomponents/NavigationBarComponent.jsx
+++ src/client/src/Components/Header/Subcomponents/NavigationBarComponent.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import logo from '../../../Images/flightLogo.png';
 
 class NavigationBarComponent extends Component {
 
@@ -8,16 +9,37 @@ class NavigationBarComponent extends Component {
         this.state = {
         }
 
+        this.loginUser = this.loginUser.bind(this);
     }
 
     componentDidMount() {
     }
 
+    loginUser() {
+        this.props.history.push('/login')
+    }
+
     render() {
         return (
-            <div>
-
-            </div>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <a className="navbar-brand"><img className="navbar-logo" src={logo}/></a>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="navitem">
+                            <a className="navlink">trip dashboard</a>
+                        </li>
+                        <li className="navitem">
+                            <a className="navlink">passenger survey</a>
+                        </li>
+                        <li className="navitem">
+                            <a className="navlink">inflight service</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <a className="btn" href="/login">login</a>
+                    </form>
+                </div>
+            </nav>
         );
     }
 }
