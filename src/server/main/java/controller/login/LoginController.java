8ed67249700fcb5a238ccb219c22df8b283649f6
ge42faz <ge42faz@mytum.de>
2022-07-14 02:01:05 +0200

    included updates to backend api controllers (cross origin)

--- src/server/main/java/controller/login/LoginController.java
+++ src/server/main/java/controller/login/LoginController.java
@@ -14,6 +14,7 @@ import utils.userflights.UserFlightsRepository;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class LoginController {
 
